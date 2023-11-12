71a307034314befafd7e1f3540cd9a8da86b1591
afrianluthfan <55348072+afrianluthfan@users.noreply.github.com>
2023-11-13 00:34:41 +0700

    chore: animated all ui

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,31 +1,69 @@
 import React, { FC } from 'react';
-import Image from 'next/image';
+import { motion } from 'framer-motion';
+import AnimatedIcon from './AnimatedIcon';
 
-const Navbar: FC = () => (
-  <div className='flex flex-col justify-center w-screen h-8 absolute top-0'>
-    <div className='flex mt-52 mx-20 h-16 justify-between items-center'>
-      <div className='bg-white rounded-full w-20 h-20 items-center justify-center'>
-        <Image
-          src='https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-2.svg'
-          alt='placeholder-logo'
-          height={110}
-          width={110}
-          className='mt-2'
-        />
-      </div>
-      <div className='grid grid-cols-3 gap-10'>
-        <div className='px-2 py-1 hover:bg-white h-full w-full rounded-md text-l font-bold text-center text-white hover:text-black'>
-          About Us
+const Navbar: FC = () => {
+  const gerak = {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  return (
+    <div className='flex flex-col justify-center w-screen h-8 absolute top-0'>
+      <div className='flex mt-52 mx-20 h-16 justify-between items-center'>
+        <div className='relative bg-none rounded-full w-20 h-20 items-center justify-center'>
+          <AnimatedIcon />
         </div>
-        <div className='px-2 py-1 hover:bg-white h-full w-full rounded-md text-l font-bold text-center text-white hover:text-black'>
-          Contact Us
-        </div>
-        <div className='px-2 py-1 hover:bg-white h-full w-full rounded-md text-l font-bold text-center text-white hover:text-black'>
-          FAQs
+        <div className='grid grid-cols-3 gap-10'>
+          <motion.div
+            className='px-2 py-1 hover:bg-white h-full w-full rounded-md text-l font-bold text-center text-white hover:text-black'
+            variants={gerak}
+            initial='hidden'
+            animate='visible'
+            transition={{
+              delay: 0,
+              duration: 1,
+              ease: 'easeInOut',
+            }}
+          >
+            About Us
+          </motion.div>
+          <motion.div
+            className='px-2 py-1 hover:bg-white h-full w-full rounded-md text-l font-bold text-center text-white hover:text-black'
+            variants={gerak}
+            initial='hidden'
+            animate='visible'
+            transition={{
+              delay: 0.2,
+              duration: 1,
+              ease: 'easeInOut',
+            }}
+          >
+            Contact Us
+          </motion.div>
+          <motion.div
+            className='px-2 py-1 hover:bg-white h-full w-full rounded-md text-l font-bold text-center text-white hover:text-black'
+            variants={gerak}
+            initial='hidden'
+            animate='visible'
+            transition={{
+              delay: 0.4,
+              duration: 1,
+              ease: 'easeInOut',
+            }}
+          >
+            FAQs
+          </motion.div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,80 @@
+'use client';
+
 import { Button } from '@nextui-org/button';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 import Navbar from './components/Navbar';
 
-const Home = () => (
-  <>
-    <Navbar />
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-5xl font-bold mb-5 text-white'>
-        Uncover your musical aura
-      </h1>
-      <p className='text-l mb-8 block whitespace-pre text-center text-white'>
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eu
-        <br />
-        lorem et ultricies. In porta lorem at dui semper porttitor. Nullam quis
-      </p>
-      <div className='flex items-center justify-center'>
-        <Button className='font-bold' size='lg'>
-          <Image
-            src='https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg'
-            alt='spotify-icon'
-            width={30}
-            height={30}
-          />
-          Sign in with Spotify
-        </Button>
+const Home = () => {
+  const gerak = {
+    hidden: {
+      y: 50,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <motion.h1
+          className='text-5xl font-bold mb-5 text-white'
+          variants={gerak}
+          initial='hidden'
+          animate='visible'
+          transition={{
+            delay: 0,
+            duration: 2,
+            ease: 'easeInOut',
+          }}
+        >
+          Uncover your musical aura
+        </motion.h1>
+        <motion.p
+          className='text-l mb-8 block whitespace-pre text-center text-white'
+          variants={gerak}
+          initial='hidden'
+          animate='visible'
+          transition={{
+            delay: 0.5,
+            duration: 2,
+            ease: 'easeOut',
+          }}
+        >
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+          eu
+          <br />
+          lorem et ultricies. In porta lorem at dui semper porttitor. Nullam
+          quis
+        </motion.p>
+        <motion.div
+          className='flex items-center justify-center'
+          variants={gerak}
+          initial='hidden'
+          animate='visible'
+          transition={{
+            delay: 1,
+            duration: 2,
+            ease: 'easeOut',
+          }}
+        >
+          <Button className='font-bold' size='lg'>
+            <Image
+              src='https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg'
+              alt='spotify-icon'
+              width={30}
+              height={30}
+            />
+            Sign in with Spotify
+          </Button>
+        </motion.div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Home;
