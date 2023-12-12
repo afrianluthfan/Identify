ffdf2008b77ef6124363494ea003b3c226dd4dbe
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2023-12-12 18:42:20 +0700

    fix(route): type-error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth/next';
 import { authOptions } from '@/server/authOptions';
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
