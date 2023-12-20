5679ab7b40661326b1f20c0b830a091fed75a63a
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2023-12-20 20:38:30 +0700

    fix: footer

--- src/components/sections/PageFooter.tsx
+++ src/components/sections/PageFooter.tsx
@@ -1,65 +1,65 @@
 import React, { FC } from 'react';
-import { Github, Linkedin } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
+import { buttonVariants } from '../ui/button';
+import Logo from '../Logo';
 
 const PageFooter: FC = () => (
-  <footer
-    id='footer'
-    className='flex items-center justify-between px-24 w-full h-56 border-t border-black dark:border-white bg-black/10 dark:bg-white/10'
-  >
-    <div className='pl-8 w-96 border-black dark:border-white border-l-1'>
-      <p className='text-left font-extralight text-black dark:text-white'>
-        A collaborative project by
-        <br />
-        <a className='font-medium' href='https://github.com/Aliezan'>
-          Muhammad{' '}
-          <span className='text-[#1d8d4c] dark:text-[#07F468]'>Alieza</span>{' '}
-          Nuriman
-        </a>
-        <br />
-        and
-        <br />
-        <a className='font-medium' href='https://github.com/afrianluthfan'>
-          <span className='text-[#1d8d4c] dark:text-[#07F468]'>Afrian</span>{' '}
-          Luthfan
-        </a>
-      </p>
-    </div>
-    <div className='pr-8'>
-      <p className='font-bold mb-4 text-black dark:text-white'>
-        Contact <span className='text-[#1d8d4c] dark:text-[#07F468]'>Us!</span>
-      </p>
-      <Link
-        className='flex font-extralight my-1  text-black dark:text-white'
-        href='https://github.com/Aliezan/spotify-generative-ai'
-      >
-        <Github className='mr-2' />
-        Our&nbsp;
-        <span className='font-medium text-[#1d8d4c] dark:text-[#07F468]'>
-          GitHub repository
-        </span>
-        &nbsp;for this project
-      </Link>
-      <Link
-        className='flex font-extralight my-1 text-black dark:text-white'
-        href='https://www.linkedin.com/in/muhammad-alieza-nuriman/'
-      >
-        <Linkedin className='mr-2' />
-        Alieza&apos;s&nbsp;
-        <span className='font-medium text-[#1d8d4c] dark:text-[#07F468]'>
-          LinkedIn
-        </span>
-      </Link>
-      <Link
-        className='flex font-extralight my-1 text-black dark:text-white'
-        href='https://www.linkedin.com/in/afrian-luthfan/'
-      >
-        <Linkedin className='mr-2' />
-        Afrian&apos;s&nbsp;
-        <span className='font-medium text-[#1d8d4c] dark:text-[#07F468]'>
-          LinkedIn
-        </span>
-      </Link>
+  <footer className='flex h-[120px] w-full items-center justify-center'>
+    <div className='w-full flex gap-[100px] p-[60px] ml-[40px]'>
+      <div>
+        <Logo />
+        <p className='text-tiny mt-1'>Identify your musical affinities</p>
+        <Link
+          className={`${buttonVariants({
+            variant: 'outline',
+          })} mt-4 text-tiny`}
+          href='https://github.com/Aliezan/identify'
+        >
+          <div className='dark:hidden mr-2'>
+            <Image src='./github-mark.svg' height={15} width={15} alt='gh' />
+          </div>
+          <div className='hidden dark:block mr-2'>
+            <Image
+              src='./github-mark-white.svg'
+              height={15}
+              width={15}
+              alt='gh'
+            />
+          </div>
+          GitHub Repository
+        </Link>
+      </div>
+      <div className='flex gap-10'>
+        <div>
+          <p className='text-small dark:text-white/60 text-black/60'>About</p>
+          <div className='mt-2 space-y-1'>
+            <Link className='hover:underline block text-tiny' href='/about'>
+              About this app
+            </Link>
+            <Link className='hover:underline text-tiny block' href='/privacy'>
+              Privacy Policy
+            </Link>
+          </div>
+        </div>
+        <div>
+          <p className='text-small dark:text-white/60 text-black/60'>Support</p>
+          <div className='mt-2 space-y-1'>
+            <Link className='hover:underline block text-tiny' href='/about'>
+              Buy us a coffee
+            </Link>
+            <Link className='hover:underline text-tiny block' href='/privacy'>
+              Inquiries
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className='space-y-3 mt-10 ml-[610px]'>
+        <p className='dark:text-white/60 text-black/60 text-tiny'>
+          &copy; 2023 all rights reserved.
+        </p>
+      </div>
     </div>
   </footer>
 );
