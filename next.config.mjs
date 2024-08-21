752c89f50fc93f5e9f325f2e1e3268becc8c6b30
Alieza Nuriman <115769623+Aliezan@users.noreply.github.com>
2024-08-21 17:03:09 +0700

    chore: refactor

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import withPlaiceholder from '@plaiceholder/next';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        pathname: '/image/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
+
+export default withPlaiceholder(config);
--- src/utils/Font.ts
+++ src/utils/Font.ts
@@ -4,16 +4,16 @@ import localFont from 'next/font/local';
 export const lexn = Urbanist({ weight: ['400', '600'], subsets: ['latin'] });
 
 export const StretchPro = localFont({
-  src: '../app/fonts/StretchPro.otf',
+  src: '../fonts/StretchPro.otf',
   display: 'swap',
 });
 
 export const cLight = localFont({
-  src: '../app/fonts/CruyffSans-Light.woff2',
+  src: '../fonts/CruyffSans-Light.woff2',
   display: 'swap',
 });
 
 export const cExtended = localFont({
-  src: '../app/fonts/CruyffSansExpanded-Heavy.woff2',
+  src: '../fonts/CruyffSansExpanded-Heavy.woff2',
   display: 'swap',
 });
